c8c3a294d477c0ddbaefffb0a15f1f8925261117
varunfaang21-gif <varunfaang21@gmail.com>
2026-01-01 18:59:30 +0530

    Add files via upload

---
+++ movezero2.py
@@ -0,0 +1,15 @@
+from typing import List
+class solution:
+    def movezero(self,nums:List[int])->None:
+        writepos=0
+        for i in range(len(nums)):
+            if nums[i]!=0:
+                nums[writepos]=nums[i]
+                writepos+=1
+        while writepos<len(nums):
+            nums[writepos]=0
+            writepos+=1
+nums=[0,1,0,3,12]
+obj=solution()
+obj.movezero(nums)
+print(nums)
